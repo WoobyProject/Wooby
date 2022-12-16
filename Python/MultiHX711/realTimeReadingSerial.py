964194f09a36f7c427145d8c3a37ece769fed236
pascal <pascalheude@hotmail.com>
2022-12-16 17:27:50 +0100

    Change real time reading to allow the reading of the 3 sensors

--- Python/MultiHX711/realTimeReadingSerial.py
+++ Python/MultiHX711/realTimeReadingSerial.py
@@ -43,18 +43,20 @@ myWooby.tare()
 # Create figure for plotting
 import matplotlib.gridspec as gridspec
 fig = plt.figure()
-gs = gridspec.GridSpec(2, 2)
+gs = gridspec.GridSpec(2, 3)
 ax = fig.add_subplot(gs[0, :])
 ax_s1 = fig.add_subplot(gs[1, 0])
 ax_s2 = fig.add_subplot(gs[1, 1])
+ax_s3 = fig.add_subplot(gs[1, 2])
 ax2 = ax.twinx()
 xs = [] #store trials here (n)
 ys1 = [] #store relative frequency here
 ys2 = []
+ys3 = []
 cvF = []
 
 # This function is called periodically from FuncAnimation
-def animate(i, xs, ys1, ys2, cvF, myWooby):
+def animate(i, xs, ys1, ys2, ys3, cvF, myWooby):
     # Aquire and parse data from serial port
     """
     line = ser.readline()  # ascii
@@ -70,6 +72,7 @@ def animate(i, xs, ys1, ys2, cvF, myWooby):
     xs.append(dfDualLoadCell['tBeforeMeasure1']/1000)
     ys1.append(dfDualLoadCell['relativeVal_WU1'])  # realValue_WU1
     ys2.append(dfDualLoadCell['relativeVal_WU2'])
+    ys3.append(dfDualLoadCell['relativeVal_WU3'])
     cvF.append(dfDualLoadCell['correctedValueFiltered'])
 
     # Limit x and y lists to 20 items
@@ -80,13 +83,16 @@ def animate(i, xs, ys1, ys2, cvF, myWooby):
     ax.clear()
     ax_s1.clear()
     ax_s2.clear()
+    ax_s3.clear()
     ax.plot(xs, ys1, label="Sensor 1", c="blue")
-    ax.plot(xs, ys2, label="Sensor 2", c="orange")
+    ax.plot(xs, ys2, label="Sensor 2", c="red")
+    ax.plot(xs, ys3, label="Sensor 3", c="green")
     ax_s1.plot(xs, ys1, label="Sensor 1", c="blue")
-    ax_s2.plot(xs, ys2, label="Sensor 2", c="orange")
+    ax_s2.plot(xs, ys2, label="Sensor 2", c="red")
+    ax_s3.plot(xs, ys3, label="Sensor 3", c="green")
 
     ax2.clear()
-    ax2.plot(xs,cvF, 'r', label="Display value")
+    ax2.plot(xs,cvF, 'o', label="Display value")
     #ax.plot(xs, rs, label="Theoretical Probability")
 
     # Format plot
@@ -98,11 +104,12 @@ def animate(i, xs, ys1, ys2, cvF, myWooby):
     ax.grid(True)
     ax_s1.grid(True)
     ax_s2.grid(True)
+    ax_s3.grid(True)
     # plt.axis([1, None, 0, 1.1])  # Use for arbitrary number of trials
     # plt.axis([1, 100, 0, 1.1]) #Use for 100 trial demo
 
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys1, ys2, cvF, myWooby), interval=1500)
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys1, ys2, ys3, cvF, myWooby), interval=1500)
 plt.show()
 
